448cf17c1789f797b9e684dc4f4056c15bff9c1e
Carlos Rocha <carlosrocha1896@gmail.com>
2025-03-20 20:37:21 -0300

    realização da cobertura de testes

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,8 +10,10 @@ def test_get_task():
     assert get_task(1) is not None
 
 def test_update_task():
-    updated_task = Task(id=1, title=1, description="Updated", completed="True")
-    assert update_task(1, updated_task) == updated_task
+    updated_task = Task(id=1, title="Updated Task", description="Updated description", completed=True)  
+    assert update_task(1, updated_task) == updated_task  
+
 
 def test_delete_task():
-    assert delete_task(1) == {"message": "Task deleted com sucesso"}
+    
+    assert delete_task(1) == {"message": "Task deleted com sucesso!"}
