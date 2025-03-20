1db61cfb4297af4df270029030f04c1abac09413
João Pedro Costa Evangelista <joaopedrocostaevangelista@MacBook-Pro-de-Joao.local>
2025-03-20 20:38:32 -0300

    joao pedro e larissa

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-## /app/main.py
 from fastapi import FastAPI, HTTPException
 from app.models import Task
 from app.database import tasks
@@ -6,18 +5,26 @@ from app.services import get_task, add_task, update_task, delete_task
 
 app = FastAPI()
 
+
+# Rota inicial
 @app.get("/")
 def read_root():
     return {"message": "Bem-vindo à API de Tarefas!"}
 
+
+# Listar todas as tarefas
 @app.get("/tasks")
 def list_tasks():
     return tasks
 
+
+# Criar uma nova tarefa
 @app.post("/tasks")
 def create_task(task: Task):
     return add_task(task)
 
+
+# Ler uma tarefa específica pelo ID
 @app.get("/tasks/{task_id}")
 def read_task(task_id: int):
     task = get_task(task_id)
@@ -25,11 +32,19 @@ def read_task(task_id: int):
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
+
+# Atualizar uma tarefa existente
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    return update_task(task_id)
+def update_task_route(task_id: int, task: Task):
+    updated_task = update_task(task_id, task)
+    if not updated_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return updated_task
 
+
+# Deletar uma tarefa pelo ID
 @app.delete("/tasks/{task_id}")
-def remove_task(task_id: int):
-    return remove_task(task_id)
-
+def remove_task_route(task_id: int):
+    if not delete_task(task_id):
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted com sucesso!"}  
